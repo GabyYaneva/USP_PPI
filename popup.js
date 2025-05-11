1cda9e3e3e3101f8210b7d08a6c2509d70d7ee1d
GabyYaneva <s22621642@onlineedu.tu-varna.bg>
2025-05-11 13:48:36 +0300

    final

--- popup.js
+++ popup.js
@@ -1,16 +1,21 @@
-
 const userIcon = document.getElementById('user-icon');
 const popup = document.getElementById('registration-popup');
-const closePopup = document.getElementById('close-popup');
+const closeBtn = document.getElementById('close-popup');
+
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const switchToRegister = document.getElementById('register-link');
+const switchToLogin = document.getElementById('login-link');
 
 
-userIcon.addEventListener('click', () => {
-    popup.style.display = 'flex'; 
+userIcon?.addEventListener('click', () => {
+    popup.style.display = 'flex';
+    showLoginForm();
 });
 
 
-closePopup.addEventListener('click', () => {
-    popup.style.display = 'none'; 
+closeBtn?.addEventListener('click', () => {
+    popup.style.display = 'none';
 });
 
 
@@ -19,3 +24,35 @@ window.addEventListener('click', (e) => {
         popup.style.display = 'none';
     }
 });
+
+
+switchToRegister?.addEventListener('click', (e) => {
+    e.preventDefault();
+    showRegisterForm();
+});
+
+switchToLogin?.addEventListener('click', (e) => {
+    e.preventDefault();
+    showLoginForm();
+});
+
+function showLoginForm() {
+    loginForm.style.display = 'flex';
+    registerForm.style.display = 'none';
+}
+
+function showRegisterForm() {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'flex';
+}
+
+registerForm?.addEventListener('submit', function (e) {
+    const password = document.getElementById('register-password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        e.preventDefault();
+        alert('Паролите не съвпадат!');
+    }
+});
+ 
